396edce785a83e67a31334429c6bf9c3470a807d
yz <zhangy627c@gmail.com>
2024-12-12 17:39:26 +0800

    12.12 结构体拷贝工具函数

---
+++ utils/copy/struct_copy.go
@@ -0,0 +1,32 @@
+package copy
+
+import "reflect"
+
+// StructCopy 拷贝结构体,不指定具体类型，解耦
+// from 和 to 必须为指针类型
+func StructCopy(from, to any) {
+	fValue := reflect.ValueOf(from)
+	tValue := reflect.ValueOf(to)
+
+	// 判断是否为指针类型
+	if fValue.Kind() != reflect.Ptr || tValue.Kind() != reflect.Ptr {
+		return
+	}
+	// 如果为空
+	if fValue.IsNil() || tValue.IsNil() {
+		return
+	}
+
+	fElem := fValue.Elem()
+	tElem := tValue.Elem()
+
+	for i := 0; i < tElem.NumField(); i++ {
+		tField := tElem.Type().Field(i)
+		fField, ok := fElem.Type().FieldByName(tField.Name)
+
+		// 字段名称相同，类型相同
+		if ok && fField.Type == tField.Type {
+			tElem.Field(i).Set(fElem.FieldByName(tField.Name))
+		}
+	}
+}
